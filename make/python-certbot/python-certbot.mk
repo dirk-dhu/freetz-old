5852546124b26668a8992a28e8253eb8a8ed3c8c
Dirk <dirk-dhu@users.noreply.github.com>
2017-02-19 17:21:10 +0100

    add letsencrypt (certbot scripts) to create ssl certificates

--- make/python-certbot/python-certbot.mk
+++ make/python-certbot/python-certbot.mk
@@ -1,10 +1,14 @@
-$(call PKG_INIT_BIN, 82ac89b)
-$(PKG)_SOURCE:=certbot-$($(PKG)_VERSION).tar.gz
-$(PKG)_SITE:=git@https://github.com/certbot/certbot.git
+$(call PKG_INIT_BIN, 0.22.2)
+#$(PKG)_SOURCE:=certbot-$($(PKG)_VERSION).tar.gz
+#$(PKG)_SITE:=git@https://github.com/certbot/certbot.git
+$(PKG)_SOURCE:=v$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=00cd4517bd2840d80ec45be2c8227fe4
+$(PKG)_SITE:=https://github.com/certbot/certbot/archive/
 
 $(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/certbot-$($(PKG)_VERSION)
 
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/certbot
+$(PKG)_TARGET_HTTP_AUTH:=$($(PKG)_DEST_DIR)/usr/lib/certbot/tests/manual-http-auth.sh $($(PKG)_DEST_DIR)/usr/lib/certbot/tests/manual-http-cleanup.sh $($(PKG)_DEST_DIR)/usr/lib/certbot/tests/run_http_server.py
 
 $(PKG)_DEPENDS_ON += python
 
@@ -35,18 +39,23 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
 		TARGET_ARCH=$(FREETZ_TARGET_ARCH), \
 		./setup.py install --prefix=/usr --root=$(FREETZ_BASE_DIR)/$(PYTHON_CERTBOT_DEST_DIR) \
 	); \
-	for f in ls $(FREETZ_BASE_DIR)/$(PYTHON_CERTBOT_DEST_DIR)/usr/bin/*; do sed -i -r -e 's~^#!/.*/hostpython~#!/usr/bin/python~g' $$f; done
+	for f in ls $(FREETZ_BASE_DIR)/$(PYTHON_CERTBOT_DEST_DIR)/usr/bin/*; do sed -i -r -e 's~^#!/.*/hostpython~#!/usr/bin/python~g' $$f; done; \
+
+$($(PKG)_TARGET_HTTP_AUTH): $($(PKG)_TARGET_BINARY)
+	mkdir -p ${@D}; \
+	cp $(PYTHON_CERTBOT_DIR)/tests/$(@F) $@
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_HTTP_AUTH)
 
 $(pkg)-clean:
-	$(RM) -r $(PYTHON_CERTBOT_DIR)/build
+	$(RM) -r $(PYTHON_CERTBOT_DIR)/build 
 
 $(pkg)-uninstall:
 	$(RM) -r \
 		$(PYTHON_CERTBOT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/certbot \
-		$(PYTHON_CERTBOT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/certbot-*.egg-info
+		$(PYTHON_CERTBOT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/certbot-*.egg-info \
+		$(PYTHON_CERTBOT_TARGET_HTTP_AUTH)
 
 $(PKG_FINISH)
